90b142b0e805160390a5fa61a1d5f07e1b261d8a
Kirti Bijay Padhi <39792869+kirtibijaypadhi@users.noreply.github.com>
2023-06-02 22:47:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,144 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+//Music 
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Goodnight, Disco Queen',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Front Row',
+        artist: 'Darkest Void',
+    }
+];
+
+// Check if playing
+let isPlaying = false;
+
+// Play
+function playSong(){
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+// pause
+function pauseSong(){
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+
+// plau or pause event
+playBtn.addEventListener('click', ()=> (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song){
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src=`music/${song.name}.mp3`;
+    image.src=`img/${song.name}.jpg`;
+}
+
+// Current song
+let songIndex = 0;
+
+// prev song
+function prevSong(){
+    songIndex--;
+    if(songIndex < 0)
+    {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+
+// Next song
+function nextSong(){
+    songIndex++;
+    if(songIndex > songs.length -1 )
+    {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+
+//updates progress bar
+function updateProgressBar(e){
+    if(isPlaying)
+    {
+        const {duration, currentTime} = e.srcElement;
+        // update progress bar
+        const progressPercent = (currentTime/duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+        // calculate the display
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60);
+        if(durationSeconds < 10)
+        {
+            durationSeconds = `0${durationSeconds}`;
+        }
+        
+        // delay switching duration element to avoid NAN
+        if(durationSeconds)
+        {
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+        // calculate the display current
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if(currentSeconds < 10)
+        {
+            currentSeconds = `0${currentSeconds}`;
+        }
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+    }
+}
+
+//set progress bar
+function setProgressBar(e){
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const {duration} = music;
+    music.currentTime = (clickX / width) * duration;
+}
+
+
+// event listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
